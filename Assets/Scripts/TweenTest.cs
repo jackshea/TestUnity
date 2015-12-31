7c71afcec3c11a41bd36ab3307c9c5b5fcfa6742
jackshea <jackshea@163.com>
2015-12-31 23:08:29 +0800

    dotween文字测试
    Signed-off-by: jackshea <jackshea@163.com>

---
+++ Assets/Scripts/TweenTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class TweenTest : MonoBehaviour
+{
+    // Use this for initialization
+    Text text;
+    void Start()
+    {
+        text = GetComponent<Text>();
+        text.DOText("Hello,world.", 5f,true,ScrambleMode.All);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
