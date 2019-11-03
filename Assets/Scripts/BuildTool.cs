fabcc8d1600e5630c55b9753a9c581baac75b92b
谢在奎 <jackshea@163.com>
2019-11-03 21:27:55 +0800

    升级为Unity2018.2.8

---
+++ Assets/Scripts/BuildTool.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+/// 打包工具
+public class BuildTool
+{
+    [MenuItem("Tools/构建/Android平台")]
+    public static void BuildAndroid()
+    {
+        var buildTarget = BuildTarget.Android;
+        string rootPath = "Build";
+        //string build = $"{DateTime.Now:yyMMddHHmmss}";
+        string outputPath = Path.Combine(rootPath, "Android");
+        string filename = outputPath + $"\\SunlightRock.apk";
+        
+        if (File.Exists(filename))
+        {
+            File.Delete(filename);
+        }
+
+        Directory.CreateDirectory(outputPath);
+        EditorUserBuildSettings.androidBuildSystem = AndroidBuildSystem.Gradle;
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, filename, buildTarget, BuildOptions.None);
+        Debug.Log(buildTarget + "平台构建完成.  文件位于" + filename);
+    }
+
+    [MenuItem("Tools/构建/iOS平台")]
+    public static void BuildiOS()
+    {
+        var buildTarget = BuildTarget.iOS;
+        string rootPath = "Build";
+        //string build = $"{DateTime.Now:yyMMddHHmmss}";
+        string outputPath = Path.Combine(rootPath, "iOS");
+
+        if (Directory.Exists(outputPath))
+        {
+            Directory.Delete(outputPath, true);
+        }
+
+        Directory.CreateDirectory(outputPath);
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, outputPath, buildTarget, BuildOptions.None);
+        Debug.Log(buildTarget + "平台构建完成.  文件位于" + outputPath);
+    }
+}
